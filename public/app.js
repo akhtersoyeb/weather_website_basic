d2adb2ce8c19ddddb931c92c1bfaad8dc4d88a17
sk soyeb akhter <saleramesty2@gmail.com>
2020-09-06 20:51:01 +0530

    replaced weather stack api with open weather map api for high accuracy button

--- public/app.js
+++ public/app.js
@@ -11,14 +11,15 @@ highAccuracyBtn.addEventListener('click', () => {
 
             let response = await fetch(`highAccuracyWeather/${lat},${lon}`);
             let data = await response.json();
+            // console.log(data);
 
-            let temperature = data.current.temperature;
-            let weather_icon = data.current.weather_icons[0];
-            let weather_description = data.current.weather_descriptions[0];
-            let location_name = data.location.name;
-            let location_region = data.location.region;
+            let temperature = data.main.temp;
+            let weather_icon = data.weather['0'].icon;
+            let weather_description = data.weather['0'].description;
+            let location_name = data.name;
 
-            console.log(temperature, weather_icon, weather_description, location_name, location_region);
+
+            console.log(temperature, weather_icon, weather_description, location_name);
         })
     } else {
         console.log('position is unavailable');
@@ -33,14 +34,14 @@ highAccuracyBtn.addEventListener('click', () => {
 async function getWeatherData() {
     let response = await fetch('/weather');
     let data = await response.json();
-
+    // console.log(data);
     let temperature = data.current.temperature;
     let weather_icon = data.current.weather_icons[0];
     let weather_description = data.current.weather_descriptions[0];
     let location_name = data.location.name;
-    let location_region = data.location.region;
 
-    console.log(temperature, weather_icon, weather_description, location_name, location_region);
+
+    console.log(temperature, weather_icon, weather_description, location_name);
 }
 
 
--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.listen(port, () => {
 });
 
 
-const api_key = '7c3d00941c27d3b07020e56481c99d19';
+const api_key_weather_stack = '7c3d00941c27d3b07020e56481c99d19';
+const api_key_open_weather_map = '9dd5b799f3bc67aa379bb2b47a558d96';
 
 app.get('/weather', async (request, response) => {
-    const api_url = `http://api.weatherstack.com/current?access_key=${api_key}&query=fetch:ip`;
+    // const api_url = `http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=9dd5b799f3bc67aa379bb2b47a558d96`;
+    const api_url = `http://api.weatherstack.com/current?access_key=${api_key_weather_stack}&query=fetch:ip`;
     const fetch_response = await fetch(api_url);
     const json = await fetch_response.json();
     response.json(json);
@@ -23,7 +25,7 @@ app.get('/highAccuracyWeather/:latlon', async (request, response) => {
     latlon = request.params.latlon.split(',');
     lat = latlon[0];
     lon = latlon[1];
-    const api_url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${lat},${lon}`;
+    const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key_open_weather_map}`;
     const fetch_response = await fetch(api_url);
     const json = await fetch_response.json();
     response.json(json);
